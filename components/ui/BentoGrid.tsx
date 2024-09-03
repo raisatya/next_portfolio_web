0ed4ef7d165b050e220395e9f0ce79cf5439a4eb
raisatya <satyanlpid1@gmail.com>
2024-09-03 22:22:02 +0530

    Changed Email Id and Name

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -68,7 +68,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "satyanlpid1@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
